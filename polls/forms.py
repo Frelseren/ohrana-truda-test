72f042cbd2598e1d5911b345093ebd6e5811a09e
Nikita Verkhoshintcev <verkhoshintsev@gmail.com>
2016-11-16 21:57:23 +0300

    testing dynamic vars

--- polls/forms.py
+++ polls/forms.py
@@ -4,3 +4,4 @@ class PostForm(forms.Form):
     # question1 = forms.IntegerField(required=True)
     for i in range(1,201):
         globals()['question{0}'.format(i)] = forms.IntegerField(required=True)
+    print(question1)
